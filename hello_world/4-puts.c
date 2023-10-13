21c1c7bbd5d012d9e556d3bb0493d684425cb666
MagicEmperador <jose_duarte@outlook.fr>
2023-10-13 01:34:54 -0700

    fonction puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+char Mystr1[10] = "Programming is like building a multilingual puzzle";
+puts(Mystr1);
+return (0);
 }
