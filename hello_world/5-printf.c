4134878e868340ac063c1755134107031734f4c2
MagicEmperador <jose_duarte@outlook.fr>
2023-10-13 02:16:12 -0700

    fonction printf langage c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char chaine[] = "with proper grammar, but the outcome is a piece of art\,";
+printf(chaine);
+
+return (0);
+}
