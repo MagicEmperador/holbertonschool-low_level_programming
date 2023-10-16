81f4d474db87b1e117987e359bee049047c2a5b8
MagicEmperador <jose_duarte@outlook.fr>
2023-10-16 02:14:55 -0700

    attribuer un nombre aléatoire a une variable

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("(n) is positive\n");
+printf("%d is positive\n", number);
 }
 else if (n == 0)
 {
-printf("(n) is zero\n");
+printf("%d is zero\n", number);
 }
 else
 {
-printf("(n) is negative\n");
+printf("%d is negative\n", number);
 }
 
 return (0);
