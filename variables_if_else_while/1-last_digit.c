b4f342ce49087f3d7e5eb4c2c1367481786b0d2a
MagicEmperador <jose_duarte@outlook.fr>
2023-10-16 05:59:32 -0700

    afficher le dernier chiffre du nombre stocké de la variable

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,14 +1,38 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Description: affiche le dernier chiffre du nombre stocké dans la variable
+*
+* Return: si chiffre le chiffre est plus petit que 6
+* ou plus grand 5 ou egale 0 (Success)
+*/
+
 int main(void)
 {
-	int n;
+int n;
+int lastnum;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastnum = n % 10;
+
+if (lastnum > 5)
+{
+printf("last digit of %d is %d and is greater than 5\n", n, lastnum);
+}
+else if (lastnum == 0)
+{
+printf("last digit of %d is %d and is 0\n", n, lastnum);
+}
+else if (lastnum < 6 && lastnum != 0)
+{
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastnum);
+}
+
+return (0);
 }
